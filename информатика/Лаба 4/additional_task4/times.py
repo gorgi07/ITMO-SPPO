b8d81e0dbf70b711f9208e5a2b9add9b083b4d96
gorgi07 <151548860+gorgi07@users.noreply.github.com>
2024-11-20 15:05:01 +0300

    Add files via upload

--- информатика/Лаба 4/additional_task4/times.py
+++ информатика/Лаба 4/additional_task4/times.py
@@ -18,7 +18,7 @@ for i in range(100):
     parser = YamlToXmlLibs(PATH)
     parser.main()
 end_time = time.perf_counter()
-print(f"Дополнительное задание №1  - {end_time - start_time}")
+print(f"Дополнительное задание №1 - {end_time - start_time}")
 
 # ----------------------- №2 -----------------------
 start_time = time.perf_counter()
@@ -27,12 +27,13 @@ for i in range(100):
     parser = YamlToXmlParserRegex(PATH)
     parser.convert_to_xml()
 end_time = time.perf_counter()
-print(f"Дополнительное задание №2  - {end_time - start_time}")
+print(f"Дополнительное задание №2 - {end_time - start_time}")
 
 # ----------------------- №3 -----------------------
 start_time = time.perf_counter()
 for i in range(100):
-    from additional_task3.yaml_to_xml_pro import yaml_to_xml
-    yaml_to_xml("C:/Users/erokh/PycharmProjects/Lab4/data/new.yaml")
+    from additional_task3.yaml_to_xml_pro import YamlToXmlParserPro
+    parser = YamlToXmlParserPro(PATH)
+    parser.convert_to_xml()
 end_time = time.perf_counter()
-print(f"Дополнительное задание №3  - {end_time - start_time}")
+print(f"Дополнительное задание №3 - {end_time - start_time}")
