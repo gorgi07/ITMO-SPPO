f8b359bed1fcfbbefe5e9a6ed5ad0bd1e0884856
gorgi07 <151548860+gorgi07@users.noreply.github.com>
2024-11-20 15:04:26 +0300

    Add files via upload

--- информатика/Лаба 4/additional_task3/yaml_to_xml_pro.py
+++ информатика/Лаба 4/additional_task3/yaml_to_xml_pro.py
@@ -1,123 +1,150 @@
-import os
+import os.path
 
 
-def parse_yaml(yaml_content):
-    lines = yaml_content.split('\n')
-    result = {}
-    stack = [(result, -1)]  # (current_dict_or_list, indent_level)
+class YamlToXmlParserPro:
+    def __init__(self, yaml_file: str) -> None:
+        self.__yaml_doc = self.__yaml_str_editing(yaml_file)
 
-    for line in lines:
-        if not line.strip() or line.strip().startswith('#'):
-            continue  # Пропускаем пустые строки и комментарии
+    @staticmethod
+    def __get_yaml_str(yaml_file: str) -> str:
+        """Получаем строку из YAML файла"""
+        with open(yaml_file, "r", encoding="utf-8") as read_file:
+            yaml_text = read_file.read()
+        return yaml_text
 
-        indent = len(line) - len(line.lstrip())
-        stripped_line = line.lstrip()
-        if ':' in stripped_line:
-            key, value = stripped_line.split(':', 1)
-            key = key.strip()
-            value = value.strip() if value else None
-        else:
-            key = stripped_line.strip()
-            value = None
+    def __yaml_str_editing(self, yaml_file) -> list:
+        """Функция для удаления лишних отступов в YAML тексте"""
+        yaml_doc = [i for i in self.__get_yaml_str(yaml_file).split('\n')]
+        last_indent_split = None
+        for i in range(len(yaml_doc)):
+            if yaml_doc[i].strip().startswith("#"):
+                yaml_doc[i] = ""
+            elif "#" in yaml_doc[i]:
+                yaml_doc[i] = yaml_doc[i].split("#")[0].rstrip()
+        yaml_doc = [i for i in yaml_doc if i != ""]
+        for i in range(len(yaml_doc)):
+            if yaml_doc[i].strip().startswith("- "):
+                indent = (len(yaml_doc[i]) - len(yaml_doc[i].replace("-", " ").strip())) // 2
+                if last_indent_split is None or indent > last_indent_split:
+                    yaml_doc[i] = yaml_doc[i][2:]
+                    for j in range(i + 1, len(yaml_doc)):
+                        new_indent = (len(yaml_doc[j]) - len(yaml_doc[j].replace("-", " ").strip())) // 2
+                        if new_indent >= indent:
+                            yaml_doc[j] = yaml_doc[j][2:]
+                    last_indent_split = indent - 1
+        yaml_doc = [i for i in yaml_doc if i != ""]
+        return yaml_doc
 
-        # Удаляем элементы из стека, если отступ уменьшился
-        while stack and indent <= stack[-1][1]:
-            stack.pop()
-
-        # Получаем текущий родительский элемент
-        parent, _ = stack[-1] if stack else (None, -1)
-
-        if key.startswith('-'):
-            # Это элемент списка
-            key = key[1:].strip()
-            if isinstance(parent, dict):
-                # Начинаем новый список
-                if key not in parent:
-                    parent[key] = []
-                new_item = {}
-                parent[key].append(new_item)
-                if value:
-                    # Если есть значение, присваиваем его
-                    parent[key][-1] = value
-                else:
-                    # Добавляем новый элемент в стек
-                    stack.append((new_item, indent))
-            elif isinstance(parent, list):
-                # Добавляем в существующий список
-                new_item = {}
-                parent.append(new_item)
-                if value:
-                    parent[-1] = value
-                else:
-                    stack.append((new_item, indent))
-        else:
-            # Это элемент словаря
-            if isinstance(parent, list):
-                # Добавляем в последний словарь в списке
-                if value is not None:
-                    parent[-1][key] = value
-                else:
-                    parent[-1][key] = {}
-                    stack.append((parent[-1][key], indent))
-            elif isinstance(parent, dict):
-                if value is not None:
-                    parent[key] = value
-                else:
-                    parent[key] = {}
-                    stack.append((parent[key], indent))
+    def convert_to_xml(self, new_file=False):
+        """Записывает XML в файл"""
+        if new_file:
+            if not os.path.isfile(f"Output.xml"):
+                output_file = "Output.xml"
             else:
-                raise ValueError("Неверная структура YAML")
-
-    return result
-
-
-def dict_to_xml(data, indent=0):
-    xml = ''
-    indent_space = ' ' * indent
-
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, list):
-                for item in value:
-                    xml += f"{indent_space}<{key}>\n"
-                    xml += dict_to_xml(item, indent + 2)
-                    xml += f"{indent_space}</{key}>\n"
-            elif isinstance(value, dict):
-                xml += f"{indent_space}<{key}>\n"
-                xml += dict_to_xml(value, indent + 2)
-                xml += f"{indent_space}</{key}>\n"
-            else:
-                xml += f"{indent_space}<{key}>{value}</{key}>\n"
-    elif isinstance(data, list):
-        for item in data:
-            xml += dict_to_xml(item, indent)
-    else:
-        xml += f"{indent_space}{data}\n"
-
-    return xml
-
-
-def yaml_to_xml(yaml_file_path, new_file=False):
-    with open(yaml_file_path, 'r', encoding='utf-8') as read_file:
-        yaml_content = read_file.read()
-
-    yaml_dict = parse_yaml(yaml_content)
-    xml_content = dict_to_xml(yaml_dict)
-
-    if new_file:
-        if not os.path.isfile(f"Output.xml"):
+                i = 1
+                while os.path.isfile(f"Output ({i}).xml"):
+                    i += 1
+                output_file = f"Output ({i}).xml"
+        else:
             output_file = "Output.xml"
-        else:
-            i = 1
-            while os.path.isfile(f"Output ({i}).xml"):
-                i += 1
-            output_file = f"Output ({i}).xml"
-    else:
-        output_file = "Output.xml"
+        with open(output_file, "w", encoding="utf-8") as write_file:
+            write_file.write(self.__yaml_to_xml())
 
-    with open(output_file, 'w', encoding='utf-8') as write_file:
-        write_file.write(xml_content)
+    def __yaml_to_xml(self) -> str:
+        """Переводит текст записанный в формате YAML в XML"""
+        out = ""
+        last_tags = dict()
+        last_indent = 0
+        flag = False
+
+        # перебираем все строки
+        for i in range(len(self.__yaml_doc)):
+            keys = list(last_tags.keys())
+            element = self.__yaml_doc[i]
+            # ищем длину отступа
+            indent = (len(element) - len(element.replace("-", " ").lstrip())) // 2
+
+            # если блок закончился, то ставим закрывающийся тэг
+            if indent < last_indent:
+                for j in range(len(keys) - 1, -1, -1):
+                    if keys[j] < last_indent:
+                        if self.__yaml_doc[i - 1].strip()[0] == "-":
+                            if last_tags[last_indent - 1] != last_tags[keys[j]]:
+                                out += "\t" * keys[j] + f"</{last_tags[keys[j]]}>\n"
+                                break
+                        elif last_tags[last_indent] != last_tags[keys[j]]:
+                            out += "\t" * keys[j] + f"</{last_tags[keys[j]]}>\n"
+                            break
+
+            # Если строка содержит ':' и (следующая строка содержит '-' или состоит из двух частей (через ':')),
+            # то создаем открывающий тэг
+            if ((element.rstrip()[-1] == ":" and i != len(self.__yaml_doc) - 1 and
+                 (self.__yaml_doc[i + 1].strip()[0] != "-" or ": " in self.__yaml_doc[i + 1].strip())) or
+                    (element.rstrip()[-1] == ":" and i == len(self.__yaml_doc) - 1)):
+                content = element.replace(":", "").replace("-", "").strip()
+                out += "\t" * indent + f"<{content}>\n"
+                last_tags[indent] = content
+
+            # Если строка содержит ':' и следующая строка содержит '-',
+            # то записываем тэг
+            elif (element.rstrip()[-1] == ":" and i != len(self.__yaml_doc) - 1
+                  and self.__yaml_doc[i + 1].strip()[0] == "-"):
+                content = element.replace(":", "").replace("-", "").strip()
+                last_tags[indent] = content
+
+            # Если строка состоит из двух частей (через ':') и содержит '-',
+            # то закрываем предыдущий тэг и открываем по новой
+            # для конструкций типа:
+            #   pair:
+            #     - type: Лекция
+            #       name: Информатика
+            #     - type: Лекция
+            #       name: ОСНОВЫ ПРОФЕССИОНАЛЬНОЙ ДЕЯТЕЛЬНОСТИ
+            elif ": " in element and element.strip()[0] == "-":
+                content = [i.replace("-", "").strip() for i in element.split(": ")]
+                if flag:
+                    for j in range(len(keys) - 1, -1, -1):
+                        if keys[j] < indent:
+                            out += "\t" * keys[j] + f"</{last_tags[keys[j]]}>\n"
+                            out += "\t" * keys[j] + f"<{last_tags[keys[j]]}>\n"
+                            break
+                out += "\t" * indent + f"<{content[0]}>{content[1]}</{content[0]}>\n"
+                last_tags[indent] = content[0]
+                flag = True
+
+            # Если строка состоит из двух частей (через ':'), то записываем содержание в тэг
+            elif ": " in element:
+                content = [i.replace("-", "").strip() for i in element.split(": ")]
+                out += "\t" * indent + f"<{content[0]}>{content[1]}</{content[0]}>\n"
+                last_tags[indent] = content[0]
+
+            # Если идет блок значений с '-' в начале, то записываем каждый элемент внутри тэга
+            # для конструкций типа:
+            # week:
+            #   - 3
+            #   - 7
+            #   - 9
+            #   - 11
+            else:
+                content = element.replace("-", "").strip()
+                if element.strip()[0] == "-":
+                    out += "\t" * (indent - 1) + f"<{last_tags[indent - 1]}>{content}</{last_tags[indent - 1]}>\n"
+                else:
+                    out += "\t" * indent + f"<{last_tags[indent]}>{content}</{last_tags[indent]}>\n"
+            last_indent = indent
+
+        # Закрываем основные оставшиеся тэги
+        keys = list(last_tags.keys())
+        for j in range(len(keys) - 1, -1, -1):
+            if keys[j] < last_indent:
+                out += "\t" * keys[j] + f"</{last_tags[keys[j]]}>\n"
+        return out
 
 
-# Преобразование в XML
-yaml_to_xml("C:/Users/erokh/PycharmProjects/Lab4/data/new.yaml")
+if __name__ == '__main__':
+    try:
+        parser = YamlToXmlParserPro("C:/Users/erokh/PycharmProjects/Lab4/data/Input.yaml")
+        parser.convert_to_xml()
+        print("Конвертация успешно завершена")
+    except:
+        print("ERROR")
